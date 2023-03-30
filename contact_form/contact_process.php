f5c42da07c761d6f17474f032b0694a16a6aacc6
Artem Khmelev <artem0203@yandex.ru>
2023-03-30 12:47:49 +0300

    initial commit

---
+++ contact_form/contact_process.php
@@ -0,0 +1,70 @@
+<?php
+
+include dirname(dirname(__FILE__)).'/mail.php';
+
+error_reporting (E_ALL ^ E_NOTICE);
+
+$post = (!empty($_POST)) ? true : false;
+
+if($post)
+{
+include 'email_validation.php';
+
+$name = stripslashes($_POST['name']);
+//$email = trim($_POST['email']);
+$subject = stripslashes($_POST['subject']);
+$message = '
+Имя: '.$_POST['name'].'
+Телефон: '.$_POST['subject'].'
+';
+
+
+$error = '';
+
+// Check name
+
+if(!$name)
+{
+$error .= 'Введите имя<br />';
+}
+
+// Check email
+
+//if(!$email)
+//{
+//$error .= 'Please enter an e-mail address.<br />';
+//}
+
+//if($email && !ValidateEmail($email))
+//{
+//$error .= 'Please enter a valid e-mail address.<br />';
+//}
+
+// Check message (length)
+
+//if(!$message || strlen($message) < 10)
+//{
+//$error .= "Please enter your message. It should have at least 10 characters.<br />";
+//}
+
+
+if(!$error)
+{
+$mail = mail(CONTACT_FORM, $subject = 'Заказ звонка с сайта КАС', $message,
+     "From: ".$name." <".$email.">\r\n"
+    ."Reply-To: ".$email."\r\n"
+    ."X-Mailer: PHP/" . phpversion());
+
+if($mail)
+{
+echo 'OK';
+}
+
+}
+else
+{
+echo '<div class="notification_error">'.$error.'</div>';
+}
+
+}
+?>
